3ec69c8c8d180514a368c9657f442d948da1fe78
ChatDo <nicolas.julie@epitech.eu>
2023-10-17 20:13:34 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Product from "./components/Product.vue";
+import Navbar from "./components/Navbar.vue";
+import Accueil from "@/components/Accueil.vue";
+import Forms from "@/components/Forms.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Accueil/>
+  <Product  />
+  <Forms />
+  <Footer />
+</template>
---
+++ src/components/Forms.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="p-6 ">
+    <form novalidate="" class=" w-full max-w-3xl p-8 mx-auto space-y-6 rounded-md shadow ">
+      <h2 class="w-full text-3xl font-bold leadi">Faites vos réservations !</h2>
+      <div>
+        <label for="name" class="block mb-1 ml-1">Nom</label>
+        <input id="name" type="text" placeholder="Nom" required="" class="block w-full p-2 rounded focus:outline-none focus:ring focus:ri focus:ri dark:bg-gray-800 text-white">
+      </div>
+      <div>
+        <label for="name" class="block mb-1 ml-1">Prénom</label>
+        <input id="name" type="text" placeholder="Prénom" required="" class="block w-full p-2 rounded focus:outline-none focus:ring focus:ri focus:ri dark:bg-gray-800 text-white">
+      </div>
+      <div>
+        <label for="email" class="block mb-1 ml-1">Email</label>
+        <input id="email" type="email" placeholder="Email" required="" class="block w-full p-2 rounded focus:outline-none focus:ring focus:ri focus:ri dark:bg-gray-800 text-white">
+      </div>
+      <div>
+        <label for="message" class="block mb-1 ml-1">Informations</label>
+        <textarea id="message" type="text" placeholder="Type de structure, date, temps de location..." class="block w-full p-2 rounded autoexpand focus:outline-none focus:ring focus:ri focus:ri dark:bg-gray-800 text-white"></textarea>
+      </div>
+      <div>
+        <button type="submit" class="w-full px-4 py-2 font-bold rounded shadow focus:outline-none focus:ring hover:ring focus:ri dark:bg-blue-500 focus:ri hover:ri dark:text-gray-900">Envoyer</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
